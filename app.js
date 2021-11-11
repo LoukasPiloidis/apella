ace804d8169678fa0f9534395235ee16228d1df4
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2021-11-11 14:17:57 +0100

    server refactored 1.0

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-const { getAllMovies, searchMovie, createSession, getSession, updateSession } = require('./Server/Routes/routes.js');
+const { searchMovie, createSession, getSession, updateSession } = require('./Server/Routes/routes.js');
 const cors = require('cors');
 
 const app = express();
 
-// Some nice middlewares :)
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
 
-// Add your own middlwares here!
-
-// app.get('/movies', getAllMovies);
 app.get('/movies/:title', searchMovie);
 app.post('/session', createSession);
 app.get('/session/:id', getSession);
 app.put('/session/:id', updateSession)
 
-app.listen(3007);
+app.listen(3007, () => console.log('server up and running'));
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,50 +1,20 @@
 const axios = require('axios');
-
-const database = {
-  data: {
-    results: [
-      {
-        description: "(2006) (Video)",
-        id: "tt1436480",
-        image: "https://imdb-api.com/images/original/MV5BMTgzNzkxMzk5Nl5BMl5BanBnXkFtZTgwMTQ2MzA2MDE@._V1_Ratio0.7273_AL_.jpg",
-        resultType: "Title",
-        title: "Undefined"
-      }
-    ]
-  },
-}
+const db = require('../../database/db.json');
 
 const sessions = [];
-
-// const getAllMovies = (req, res) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.send(200, movies)
-// };
-
-// const getMovie = (req, res) => {
-//   try {
-//       const { id } = req.params;
-//       const movie = movies.fantasy.filter(item => item.id === id);
-//       res.header("Access-Control-Allow-Origin", "*");
-//       res.send(200, movie);
-//   } catch (err) {
-//       res.send(404, err.message);
-//   }
-// }
 
 const searchMovie = async (req, res) => {
   const { title } = req.params;
   // const movie = await axios.get(`https://imdb-api.com/en/API/SearchMovie/k_6hif9516/${title}`)
   res.header("Access-Control-Allow-Origin", "*");
   // res.send(200, movie.data);
-  res.send(database.data);
+  res.send(db.data);
 };
 
 const createSession = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header({ 'Content-Type': 'application/json', 'Location': '/api/session' });
   sessions.push(req.body);
-  // console.log(sessions);
   res.send(201);
 }
 
@@ -52,13 +22,11 @@ const getSession = (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   const { id } = req.params;
   const session = sessions.filter(session => session.id === parseInt(id))[0];
-  // console.log(session);
   res.status(200).send(session);
 }
 
 const updateSession = (req, res) => {
   const { movies } = req.body;
-  // console.log('log', movies[0].filmTitle);
   const { id } = req.body;
   const session = sessions.filter(film => film.id === parseInt(id) ? film.movies.push(movies[0]) : film)[0];
   const finalSession = sessions.map(film => film.id === parseInt(id) ? film = session : film);
